b2f0d913da664d45290332c95e0cac4cce3fd204
maryna33309 <68459246+marusya2299@users.noreply.github.com>
2025-04-03 21:24:55 +0200

    usesearchparams

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,38 +1,74 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
+import Loader from "../../components/Loader/Loader";
+import Error from "../../components/Error/Error";
+
 import { searchMovies } from "../../api";
 import MovieList from "../../components/MovieList/MovieList";
 
 import css from '../../pages/MoviesPage/MoviesPage.module.css';
 
-const MoviesPage = () => {
-  const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
+export default function MoviesPage() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("query") || "";
 
-  const handleSearch = async () => {
-    if (!query.trim()) return;
-    try {
-      const results = await searchMovies(query);
-      setMovies(results);
-    } catch (error) {
-      console.error(error);
+  const [inputValue, setInputValue] = useState(query); 
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    if (!query) {
+      setMovies([]);
+      return;
     }
+
+    const getMovies = async () => {
+      setLoading(true);
+      setError('');
+      
+      try {
+        const data = await searchMovies(query);
+        setMovies(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getMovies();
+  }, [query]);
+
+  if (loading) return <Loader />;
+  if (error) return <Error />;
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim() === query) return;
+    setSearchParams(inputValue ? { query: inputValue } : {});
   };
 
   return (
     <div className={css.box}>
       <h2 className={css.header}>Search Movies</h2>
-      <input
-        className={css.input}
-        type="text"
-        value={query}
-        onChange={(event) => setQuery(event.target.value)}
-        placeholder="Enter movie name"
-      />
-      <button className={css.button} onClick={handleSearch}>Search</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          className={css.input}
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+          placeholder="Enter movie name"
+        />
+        <button className={css.button} type="submit">Search</button>
+      </form>
+
       <MovieList movies={movies} />
-      
     </div>
   );
-};
-
-export default MoviesPage;
+}
